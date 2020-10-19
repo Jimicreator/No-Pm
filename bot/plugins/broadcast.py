028df157b2858a2ac8315bd8474825516f29197e
xditya <58950863+xditya@users.noreply.github.com>
2020-10-19 14:40:30 +0530

    testing

---
+++ bot/plugins/broadcast.py
@@ -0,0 +1,26 @@
+from bot.hf.fic import vhkzuoi_repliz_handler
+from bot.hf.flifi import uszkhvis_chats_ahndler
+from bot.hf.stuf import get_tle_mof_t
+from bot.sql.users_sql import get_user_id
+
+@Client.on_message(pattern="^/broadcast ?(.*)", func=lambda e: e.sender_id == bot.uid))
+async def broadcastmssg(event):
+    msgtobroadcast = event.pattern_match.group(1)
+    userstobc = get_all_users()
+    error_count = 0
+    sent_count = 0
+    for bdcast in userstobc:
+        try:
+            sent_count += 1
+            await client.send_message(int(bdcast.chat_id), msgtobroadcast)
+            await asyncio.sleep(0.2)
+        except Exception as e:
+            try:
+                 logger.info(f"Error : {error_count}\nError : {e} \nUsers : {chat_id}"
+                 )
+            except:
+                 pass
+    await tgbot.send_message(
+        event.chat_id,
+        f"Broadcast sent to {sent_count} Users.\nErrors - {error_count}\n\nAll users - {len(userstobc)}"
+        )
